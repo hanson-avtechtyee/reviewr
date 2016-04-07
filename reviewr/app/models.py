93e4a5a5800edc59fe6090b1fab82655b260fcd9
hanson-avtechtyee <khanson@avtechtyee.com>
2016-04-07 05:42:22 -0700

    few models

--- reviewr/app/models.py
+++ reviewr/app/models.py
@@ -4,10 +4,51 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 """
 
-You can use the extra Flask-AppBuilder fields and Mixin's
-
-AuditMixin will add automatic timestamp of created and modified by who
-
+AuditMixin will add automatic:
+    created_on
+    changed_on
+    created_by
+    changed_by
 
 """
-        
+
+class Document(Model, AuditMixin):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(12), unique=True, nullable=False)
+    url = Column(String(600))
+    revision = Column(String(4))
+
+    def __repr__(self):
+        return self.name
+
+class Checklist(Model, AuditMixin):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(12), unique=True, nullable=False)
+    url = Column(String(600))
+    revision = Column(String(4))
+
+    def __repr__(self):
+        return self.name
+
+class Baseline(Model, AuditMixin):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(12), unique=True, nullable=False)
+    url = Column(String(600))
+    revision = Column(String(4))
+
+    def __repr__(self):
+        return self.name
+
+class Review(Model, AuditMixin):
+    id = Column(Integer, primary_key=True)
+    baseline_id = Column(Integer, ForeignKey('baseline.id'), nullable=False)
+    baseline = relationship("Baseline")
+    document_id = Column(Integer, ForeignKey('document.id'), nullable=False)
+    document = relationship("Document")
+
+    def __repr__(self):
+        return "{0}:{1}:{2}".format(self.baseline, self.document, self.id)
+
+    def month_year(self):
+        return datetime.datetime(self.stat_date, year, self.stat_date.month, 1)
+
--- reviewr/app/views.py
+++ reviewr/app/views.py
@@ -1,7 +1,17 @@
+import logging
+import datetime
+import calendar
+
 from flask import render_template
 from flask.ext.appbuilder.models.sqla.interface import SQLAInterface
 from flask.ext.appbuilder import ModelView
 from app import appbuilder, db
+from .models import Document, Checklist, Baseline
+
+log = logging.getLogger(__name__)
+
+def month_year(value):
+    return calendar.month_name[value.month] + ' ' + str(value.year)
 
 """
     Create your Views::
@@ -16,6 +26,25 @@ from app import appbuilder, db
 
     appbuilder.add_view(MyModelView, "My View", icon="fa-folder-open-o", category="My Category", category_icon='fa-envelope')
 """
+class DocumentModelView(ModelView):
+    datamodel = SQLAInterface(Document)
+    add_columns = ['name']
+    edit_columns = ['name','url']
+
+class BaselineModelView(ModelView):
+    datamodel = SQLAInterface(Baseline)
+    add_columns = ['name']
+    edit_columns = ['name','url']
+
+class ChecklistModelView(ModelView):
+    datamodel = SQLAInterface(Checklist)
+    add_columns = ['name']
+    edit_columns = ['name','url']
+
+db.create_all()
+appbuilder.add_view(DocumentModelView, "Documents", icon="fa-folder-open-o")
+appbuilder.add_view(ChecklistModelView, "Checklist", icon="fa-folder-open-o")
+appbuilder.add_view(BaselineModelView, "Baseline", icon="fa-folder-open-o")
 
 """
     Application wide 404 error handler
